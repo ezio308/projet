599abaf23c84b841f72047d92e74d9554ac7f213
lajnefmohamedmehdi <lajnefmohamedmehdi@isg.u-tunis.tn>
2022-04-02 23:00:22 +0100

    Ajoutez des fichiers projet.

---
+++ projet/Ajouter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projet
+{
+    public partial class Ajouter : Form
+    {
+        public Ajouter()
+        {
+            InitializeComponent();
+        }
+
+        private void Ajouter_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
